8407a6e10cd828cb3588ec43d953879c9d177739
basti <bastianroeling61@gmail.com>
2026-06-07 03:39:47 +0200

    Update

--- lib/app/apollon_dashboard.dart
+++ lib/app/apollon_dashboard.dart
@@ -21,41 +21,34 @@ class _AppWrapperState extends State<ApollonDashboard> {
         height: 480,
         child: Column(
           children: [
-            Container(
-              height: 100,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(ApollonConstants.dashboardCornerRadius),
-                  topRight: Radius.circular(ApollonConstants.dashboardCornerRadius),
-                ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                width: double.infinity,
                 color: colors.surface,
-              ),
-              child: Padding(
-                padding: EdgeInsetsGeometry.symmetric(horizontal: 50),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Apollon™", style: GoogleFonts.audiowide(fontSize: 36)),
-                    IconButton(
-                      onPressed: () => {},
-                      icon: Icon(Icons.settings, size: 48),
-                    ),
-                  ],
+                child: Padding(
+                  padding: EdgeInsetsGeometry.symmetric(horizontal: 32),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Apollon™", style: GoogleFonts.audiowide(fontSize: 36)),
+                      IconButton(
+                        onPressed: () => {},
+                        icon: Icon(Icons.settings, size: 48),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
             Expanded(
+              flex: 8,
               child: Container(
                 width: double.infinity,
                 decoration: BoxDecoration(
                   image: DecorationImage(
                     image: AssetImage("assets/images/Apollon Background.png"),
                     fit: BoxFit.cover,
-                  ),
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(ApollonConstants.dashboardCornerRadius),
-                    bottomRight: Radius.circular(ApollonConstants.dashboardCornerRadius),
                   ),
                 ),
                 child: Column(children: []),
