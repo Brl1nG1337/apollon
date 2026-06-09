8f8cd4b346121aed99d5328a3e5a8c6223f697fc
basti <bastianroeling61@gmail.com>
2026-06-09 17:53:44 +0200

    Update

--- lib/core/providers/apollon_weather_provider.dart
+++ lib/core/providers/apollon_weather_provider.dart
@@ -21,7 +21,7 @@ class ApollonWeatherProvider extends ChangeNotifier {
 
     // 2. Einen Timer setzen, der alle 30 Minuten neue Daten zieht.
     // So schonst du das API-Limit von Open-Meteo und dein Pi hat immer aktuelle Daten.
-    _timer = Timer.periodic(const Duration(minutes: 3), (_) {
+    _timer = Timer.periodic(const Duration(seconds: 30), (_) {
       loadWeather();
     });
   }
