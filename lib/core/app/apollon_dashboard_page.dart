4a21b10a449b859157383d4d9fece0cee54b6302
basti <bastianroeling61@gmail.com>
2026-06-13 11:30:33 +0200

    Update

--- lib/core/app/apollon_dashboard_page.dart
+++ lib/core/app/apollon_dashboard_page.dart
@@ -26,7 +26,7 @@ class _ApollonDashboardPageState extends State<ApollonDashboardPage> with Single
     super.initState();
     _animationController = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 700),
+      duration: const Duration(milliseconds: 400),
     );
     _animation = CurvedAnimation(
       parent: _animationController,
